a00daf972d49b31b7172986a5be716c50e919e58
kholil <kholil@fisip.net>
2020-11-05 06:25:58 +0700

    add support direct image from static file

--- modules/lib/direct_image.tsx
+++ modules/lib/direct_image.tsx
@@ -2,40 +2,36 @@ import React from 'react';
 import { Image, Platform, View } from 'react-native';
 import { LibComponent } from 'esoftplay';
 
+export interface LibDirect_imageSource {
+  uri: string
+}
 export interface LibDirect_imageProps {
   style?: any
-  defaultUri?: string,
+  defaultSource?: LibDirect_imageSource | any
 }
-// defaultLocalUri?: number
 export interface LibDirect_imageState {
 
 }
 
 export default class m extends LibComponent<LibDirect_imageProps, LibDirect_imageState> {
   ref: React.RefObject<Image> = React.createRef()
-  uri = this.props.defaultUri
+  source = this.props.defaultSource || ' '
 
   constructor(props: LibDirect_imageProps) {
     super(props)
-    this.setUri = this.setUri.bind(this);
-    // this.setLocalUri = this.setLocalUri.bind(this);
+    this.setSource = this.setSource.bind(this);
   }
 
-  setUri(uri: string | any): void {
-    this.uri = uri
+  setSource(uri: any): void {
+    this.source = uri
     this.ref.current?.setNativeProps({
-      [Platform.OS == 'ios' ? 'source' : 'src']: [{ uri: uri }]
+      [Platform.OS == 'ios' ? 'source' : 'src']: [uri]
     })
   }
 
-  // setLocalUri(uri: string): void {
-  //   //@ts-ignore
-  //   this.setUri(Image.resolveAssetSource(uri))
-  // }
-
   render(): any {
     return (
-      <Image ref={this.ref} source={/* this.props.defaultLocalUri || */ { uri: this.uri }} style={this.props.style} />
+      <Image ref={this.ref} source={this.source} style={this.props.style} />
     )
   }
 }
