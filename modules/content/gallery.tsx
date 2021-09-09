08cf8137494ae38575f63b1f35f0f77787e543e4
kholil <kholil@fisip.net>
2021-09-09 09:40:48 +0700

    remove custom item gallery

--- modules/content/gallery.tsx
+++ modules/content/gallery.tsx
@@ -28,26 +28,15 @@ export default function m(props: ContentGalleryProps): any {
     <ScrollView scrollEnabled={false} style={{ flex: 1, backgroundColor: 'black', overflow: 'hidden' }} >
       <Gallery
         data={images.map((image) => (image?.image))}
-        onSwipeToClose={() => {
-          if (scale == 1)
-            LibNavigation.back()
-        }}
+        onSwipeToClose={() => { if (scale == 1) LibNavigation.back() }}
         maxScale={10}
-        renderItem={(renderItemInfo) => {
-          Image.getSize(String(renderItemInfo.item), (w, h) => {
-            renderItemInfo.setImageDimensions({ width: w, height: h })
-          })
-          return <Image style={{ flex: 1, resizeMode: 'contain' }} source={{ uri: String(renderItemInfo.item) }} />
-        }}
         disableTransitionOnScaledImage
         onScaleChange={(sc) => scale = sc}
         doubleTapScale={4}
         initialIndex={index}
       />
       <Pressable
-        onPress={() => {
-          LibNavigation.back()
-        }}
+        onPress={() => { LibNavigation.back() }}
         style={{ position: 'absolute', height: 35, alignItems: 'center', justifyContent: 'center', width: 35, borderRadius: 17.5, backgroundColor: 'rgba(0,0,0,0.3)', borderWidth: 1, borderColor: 'white', top: LibStyle.STATUSBAR_HEIGHT + 24, right: 24 }} >
         <LibIcon name='close' color="white" />
       </Pressable>
