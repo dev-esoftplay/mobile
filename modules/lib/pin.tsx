2b14caf2a7fb970c2845e9a91c8b9a9005203923
kholil <kholil@fisip.net>
2021-09-17 14:53:37 +0700

    update LibPin

--- modules/lib/pin.tsx
+++ modules/lib/pin.tsx
@@ -1,6 +1,6 @@
 // withHooks
 
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { View, TextInput } from 'react-native';
 
 
@@ -8,38 +8,52 @@ export interface LibPinProps {
   length: number,
   onChangePin: (pin: string) => void
   boxStyle?: any,
+  overrideKeyboard?: boolean,
+  pinValue?: string,
   pinStyle?: any
 }
 export default function m(props: LibPinProps): any {
   const [pin, setPin] = useState<string[]>([])
   const input = useRef<TextInput>(null)
+
+  useEffect(() => {
+    setPin(props?.pinValue?.split?.(''))
+    props.onChangePin(pin.join(''))
+  }, [props.pinValue])
+
   return (
     <View>
       <View style={{ alignItems: 'center', marginTop: 20 }} >
         <View style={{ height: 70 }} >
           <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }} >
             {
-              new Array(props.length).fill('').map((_, __) => (
-                <View key={__} style={{ height: 40, width: 40, margin: 5, borderRadius: 4, borderWidth: 0.5, borderColor: '#444', ...props.boxStyle, alignItems: 'center', justifyContent: 'center' }}>
-                  {!!pin[__] && <View style={{ height: 10, width: 10, borderRadius: 5, backgroundColor: '#444', ...props.pinStyle }} />}
+              new Array(props.length).fill('').map((_, i) => (
+                <View key={i} style={{ height: 40, width: 40, margin: 5, borderRadius: 4, borderWidth: 0.5, borderColor: '#444', ...props.boxStyle, alignItems: 'center', justifyContent: 'center' }}>
+                  {!!pin?.[i] && <View style={{ height: 10, width: 10, borderRadius: 5, backgroundColor: '#444', ...props.pinStyle }} />}
                 </View>
               ))
             }
           </View>
-          <View style={{ flexDirection: 'row', width: 60 * props.length, alignSelf: 'center', justifyContent: 'center' }} >
-            <TextInput
-              ref={input}
-              contextMenuHidden={true}
-              keyboardType='numeric'
-              style={{ opacity: 0, marginLeft: -400, width: 400 + 60 * props.length, fontSize: 0, height: 60, marginTop: -40, alignSelf: 'flex-start' }} autoFocus onChangeText={(t: string) => {
-                if (t.length == props.length) {
-                  input.current!.blur()
-                }
-                let _t: string[] = t.split('')
-                setPin(_t)
-                props.onChangePin(t)
-              }} maxLength={props.length} />
-          </View>
+          {
+            !props.overrideKeyboard &&
+            <View style={{ flexDirection: 'row', width: 60 * props.length, alignSelf: 'center', justifyContent: 'center' }} >
+              <TextInput
+                ref={input}
+                contextMenuHidden={true}
+                keyboardType='numeric'
+                secureTextEntry
+                style={{ opacity: 0, marginLeft: -400, width: 400 + 60 * props.length, fontSize: 0, height: 60, marginTop: -40, alignSelf: 'flex-start' }} 
+                autoFocus 
+                onChangeText={(t: string) => {
+                  if (t.length == props.length) {
+                    input.current!.blur()
+                  }
+                  let _t: string[] = t.split('')
+                  setPin(_t)
+                  props.onChangePin(t)
+                }} maxLength={props.length} />
+            </View>
+          }
         </View>
       </View>
     </View>
