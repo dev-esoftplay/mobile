8c2a8892f7303aa3931398ccf415c5a371ded4f2
kholil <kholil@fisip.net>
2021-10-12 12:26:51 +0700

    fix isIphoneX

--- modules/lib/style.ts
+++ modules/lib/style.ts
@@ -2,10 +2,27 @@
 
 import { Dimensions, Platform, StatusBar } from "react-native";
 
+function isIphoneX() {
+  const dimen = Dimensions.get('window');
+  return (
+    Platform.OS === 'ios' &&
+    !Platform.isPad &&
+    !Platform.isTVOS &&
+    (dimen.height === 780 ||
+      dimen.width === 780 ||
+      dimen.height === 812 ||
+      dimen.width === 812 ||
+      dimen.height === 844 ||
+      dimen.width === 844 ||
+      dimen.height === 896 ||
+      dimen.width === 896 ||
+      dimen.height === 926 ||
+      dimen.width === 926)
+  );
+}
 const { height, width } = Dimensions.get("window");
-const isIphoneX = Platform.OS === "ios" && ((height === 812 || width === 812) || (height === 896 || width === 896))
-const STATUSBAR_HEIGHT = (Platform.OS === "ios" ? isIphoneX ? 44 : 20 : StatusBar.currentHeight) || 0;
-const STATUSBAR_HEIGHT_MASTER = (Platform.OS === "ios" ? isIphoneX ? 44 : 20 : StatusBar.currentHeight) || 0;
+const STATUSBAR_HEIGHT = (Platform.OS === "ios" ? isIphoneX() ? 44 : 20 : StatusBar.currentHeight) || 0;
+const STATUSBAR_HEIGHT_MASTER = (Platform.OS === "ios" ? isIphoneX() ? 44 : 20 : StatusBar.currentHeight) || 0;
 const colorPrimary = "#3E50B4"
 const colorPrimaryDark = "#3E50B4"
 const colorAccent = "#FFF"
