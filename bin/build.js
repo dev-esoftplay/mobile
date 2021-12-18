d86c673bdb2f5980b0c1e6baed0fa989f93502df
kholil <kholil@fisip.net>
2021-12-18 15:40:39 +0700

    fix formatting

--- bin/build.js
+++ bin/build.js
@@ -256,26 +256,26 @@ yarn.lock\n\
 		});
 
 		const AppJS = `import { esp, LibNotification } from 'esoftplay';
-		import * as ErrorReport from 'esoftplay/error';
-		import * as Notifications from 'expo-notifications';
-		import React, { useEffect, useRef } from 'react';
-		import { enableFreeze, enableScreens } from 'react-native-screens';
-		const { globalIdx } = require('esoftplay/global')
-		enableScreens();
-		enableFreeze(true);
-		
-		Notifications.addNotificationResponseReceivedListener(x => LibNotification.onAction(x))
-		
-		export default function App() {
-			const Home = useRef(esp.home()).current
-		
-			useEffect(() => {
-				globalIdx.reset()
-				ErrorReport.getError()
-			}, [])
-		
-			return <Home />
-		}`;
+import * as ErrorReport from 'esoftplay/error';
+import * as Notifications from 'expo-notifications';
+import React, { useEffect, useRef } from 'react';
+import { enableFreeze, enableScreens } from 'react-native-screens';
+const { globalIdx } = require('esoftplay/global')
+enableScreens();
+enableFreeze(true);
+
+Notifications.addNotificationResponseReceivedListener(x => LibNotification.onAction(x))
+
+export default function App() {
+	const Home = useRef(esp.home()).current
+
+	useEffect(() => {
+		globalIdx.reset()
+		ErrorReport.getError()
+	}, [])
+
+	return <Home />
+}`;
 		let expoLib = [
 			'@expo/vector-icons',
 			'@react-native-async-storage/async-storage',
