7590e8ad84dd82b1a518c8aff81ba6695c77cd54
kholil <kholil@fisip.net>
2022-05-25 10:31:13 +0700

    build

--- bin/build.js
+++ bin/build.js
@@ -213,29 +213,6 @@ yarn-error.log\n\
 			console.log('.gitignore has been created');
 		});
 
-		// 		const AppJS = `import { esp, LibNotification } from 'esoftplay';
-		// import * as ErrorReport from 'esoftplay/error';
-		// import * as Notifications from 'expo-notifications';
-		// import React, { useEffect, useRef } from 'react';
-		// import { enableFreeze, enableScreens } from 'react-native-screens';
-		// const { globalIdx } = require('esoftplay/global')
-		// enableScreens();
-		// enableFreeze(true);
-
-		// Notifications.addNotificationResponseReceivedListener(x => LibNotification.onAction(x))
-
-		// export default function App() {
-		// 	const Home = useRef(esp.home()).current
-
-		// 	useEffect(() => {
-		// 		globalIdx.reset()
-		// 		ErrorReport.getError()
-		// 	}, [])
-
-		// 	return <Home />
-		// }`;
-
-
 		const AppJS = `import { esp, LibNotification } from 'esoftplay';
 import * as ErrorReport from 'esoftplay/error';
 import * as Notifications from 'expo-notifications';
