5d71ee266934f8c392e5e6ccf8fa7d5ca9ffb33e
kholil <kholil@fisip.net>
2023-09-14 08:20:01 +0700

    update useTasks

--- modules/use/tasks.ts
+++ modules/use/tasks.ts
@@ -1,37 +1,33 @@
 // useLibs
 // noPage
-import { useLazyState } from "esoftplay";
-import { useRef } from "react";
 
-export default function m<T>(task: (item: T) => Promise<void>, onDone?: () => void): [(data: T[], success?: (isSuccess: boolean) => void) => void, number] {
-  const [counter, setCounter, getCounter] = useLazyState(0)
-  const onProcess = useRef(false)
+export default function m<T>(): (task: (item: T) => Promise<void>, onDone?: () => void) => [(data: T[], success?: ((isSuccess: boolean) => void) | undefined) => void] {
+  let onProcess = false
+  return (task: (item: T) => Promise<void>, onDone?: () => void): [(data: T[], success?: (isSuccess: boolean) => void) => void] => {
+    function run(data: any[], cb?: (isSuccess: boolean) => void) {
+      if (data.length == 0) {
+        if (cb) cb?.(false);
+      } else if (onProcess == true) {
+        if (cb) cb?.(false);
+      } else {
+        onProcess = true;
+        if (cb) cb?.(true);
+        try {
+          (async () => {
+            for (let i = 0; i < data.length; i++) {
+              const item = data[i];
+              await task(item);
+              if (i == (data.length - 1)) {
+                if (onDone)
+                  onDone()
+                onProcess = false;
+              }
 
-  function run(data: any[], cb?: (isSuccess: boolean) => void) {
-    if (data.length == 0) {
-      if (cb) cb?.(false);
-    } else if (onProcess.current == true) {
-      if (cb) cb?.(false);
-    } else {
-      onProcess.current = true;
-      if (cb) cb?.(true);
-      setCounter(0)()
-      try {
-        (async () => {
-          for (let i = 0; i < data.length; i++) {
-            const item = data[i];
-            await task(item);
-            if (i == (data.length - 1)) {
-              if (onDone)
-                onDone()
-              onProcess.current = false;
             }
-            setCounter(getCounter() + 1)()
-          }
-        })()
-      } catch (err) { }
-    }
-  };
-
-  return [run, counter]
+          })()
+        } catch (err) { }
+      }
+    };
+    return [run]
+  }
 }
