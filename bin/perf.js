dedb102702e69f9020e8cbc292e12430c7ea4658
kholil <kholil@fisip.net>
2023-09-27 14:49:52 +0700

    .

--- bin/perf.js
+++ bin/perf.js
@@ -11,13 +11,13 @@ if (param == 'clear') {
         let text = fs.readFileSync('./modules/' + module + '/' + task, { encoding: 'utf8' })
         const isHooks = (/\/\/\s{0,}withHooks/g).exec(text)
         const memoiz = (/\nexport default memo\(([a-zA-Z0-9]+)\)\;/g).exec(text)
+        text = text.replace("\nimport { memo } from 'react';", "")
         if (isHooks && memoiz && memoiz.length > 1) {
           const [exdefCurrent, currentNameFunction] = memoiz
-          text = text.replace("\nimport {memo} from 'react';", "")
           text = text.replace(exdefCurrent, '')
           text = text.replace("function " + currentNameFunction + "(", "export default function " + currentNameFunction + "(")
-          fs.writeFileSync('./modules/' + module + '/' + task, text, { encoding: 'utf8' })
         }
+        fs.writeFileSync('./modules/' + module + '/' + task, text, { encoding: 'utf8' })
       })
     }
   })
@@ -32,7 +32,7 @@ if (param == 'clear') {
           const exdef = (/\nexport default function ([a-zA-Z0-9]+)\(/g).exec(text)
           if (exdef && exdef.length > 1) {
             const [exdefCurrent, currentNameFunction] = exdef
-            text = text.replace('withHooks', "withHooks\nimport {memo} from 'react';")
+            text = text.replace('withHooks', "withHooks\nimport { memo } from 'react';")
             text = text.replace(exdefCurrent, "\nfunction " + currentNameFunction + '(')
             text = text + '\nexport default memo(' + currentNameFunction + ');'
             fs.writeFileSync('./modules/' + module + '/' + task, text, { encoding: 'utf8' })
