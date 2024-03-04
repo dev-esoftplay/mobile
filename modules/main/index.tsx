345597e18081ac38056cb3716c2f8fd3599a1066
kholil <kholil@fisip.net>
2024-03-04 12:53:45 +0700

    update

--- modules/main/index.tsx
+++ modules/main/index.tsx
@@ -1,18 +1,18 @@
 // withHooks
 import React from 'react';
 import { Text, View } from 'react-native';
-export interface OtherIndexArgs {
+export interface MainIndexArgs {
 
 }
-export interface OtherIndexProps {
+export interface MainIndexProps {
 
 }
 
-export default function m(props: OtherIndexProps): any {
+export default function m(props: MainIndexProps): any {
 
   return (
     <View style={{ flex: 1, backgroundColor: "white", justifyContent: 'center', alignItems: 'center' }} >
-      <Text>Hellow Esoftplay Developer, jangan lupa bismillah dulu ଘ(੭ˊᵕˋ)੭*</Text>
+      <Text>{`Hellow Esoftplay Developer, jangan lupa ﷽ dulu ଘ(੭ˊᵕˋ)੭*`}</Text>
     </View>
   )
 }
