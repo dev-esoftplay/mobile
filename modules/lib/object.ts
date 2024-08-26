29cad2b79504fc5971bd2c34ff756d288db3eaeb
kholil <kholil@fisip.net>
2024-08-26 12:57:20 +0700

    revert libobject

--- modules/lib/object.ts
+++ modules/lib/object.ts
@@ -2,8 +2,6 @@
 
 import { update } from "immhelper";
 
-type FilterByItem = (item: any, index: number) => boolean
-
 export default class m {
   #value = undefined
 
@@ -20,82 +18,52 @@ export default class m {
     this.cursorBuilder = this.cursorBuilder.bind(this)
   }
 
-  cursorBuilder(command: string, array: any, value: any, ...values: any[]): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => this {
-    return (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => {
-      let _value = array
-      let cCursor = [cursor, ...cursors]
-      let spec = {}
-      let hasError = false
-
-      cCursor = [cursor, ...cursors].map((item) => {
-        if (typeof item == 'string') {
-          _value = _value[item]
-          return item
-        } else if (typeof item == 'function') {
-          if (Array.isArray(_value)) {
-            const idx = _value.findIndex(item)
-            if (idx > -1) {
-              _value = _value[idx]
-              return idx
-            } else {
-              hasError = true
-              return undefined
-            }
-          } else {
-            hasError = true
-            return undefined
-            // new Error("LibObject : CursorByFilter must be executed at array")
-          }
-        }
-      });
-
-      let pathToUpdate = cCursor.filter(x => x != undefined).join('.')
+  cursorBuilder(command: string, array: any, value: any, ...values: any[]): (cursor?: string | number, ...cursors: (string | number)[]) => this {
+    return (cursor?: string | number, ...cursors: (string | number)[]) => {
+      let pathToUpdate = [cursor, ...cursors].filter(x => x != undefined).join('.')
       let allValues = [value, ...values].filter(x => x != undefined)
+      let spec = {}
       if (pathToUpdate != '')
         spec = { [pathToUpdate]: [command, ...allValues] }
       else
         spec = [command, ...allValues]
-      if (hasError) {
-        console.warn("LibObject: Please check your cursor!")
-        this.#value = array
-      } else
-        this.#value = update(array, spec)
+      this.#value = update(array, spec)
       return this
     }
   }
 
-  push(value?: any, ...values: any[]): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => this {
+  push(value?: any, ...values: any[]): (cursor?: string | number, ...cursors: (string | number)[]) => this {
     return this.cursorBuilder("push", this.#value, value, ...values)
   }
 
-  unshift(value?: any, ...values: any[]): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => this {
+  unshift(value?: any, ...values: any[]): (cursor?: string | number, ...cursors: (string | number)[]) => this {
     return this.cursorBuilder("unshift", this.#value, value, ...values)
   }
 
-  splice(index: number, deleteCount: number, value?: any, ...values: any[]): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => this {
+  splice(index: number, deleteCount: number, value?: any, ...values: any[]): (cursor?: string | number, ...cursors: (string | number)[]) => this {
     return this.cursorBuilder("splice", this.#value, index, deleteCount, value, ...values)
   }
-  unset(index: number | string, ...indexs: (string | number | FilterByItem)[]): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => this {
+  unset(index: number | string, ...indexs: (string | number)[]): (cursor?: string | number, ...cursors: (string | number)[]) => this {
     return this.cursorBuilder("unset", this.#value, index, ...indexs)
   }
 
-  set(value: any): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => this {
+  set(value: any): (cursor?: string | number, ...cursors: (string | number)[]) => this {
     return this.cursorBuilder("set", this.#value, value)
   }
 
-  update(callback: (lastValue: any) => any): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => this {
+  update(callback: (lastValue: any) => any): (cursor?: string | number, ...cursors: (string | number)[]) => this {
     return this.cursorBuilder("batch", this.#value, callback)
   }
 
-  assign(obj1: any): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => this {
+  assign(obj1: any): (cursor?: string | number, ...cursors: (string | number)[]) => this {
     return this.cursorBuilder("assign", this.#value, deepCopy(obj1))
   }
 
-  removeKeys(deletedItemKeys: string[]): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => this {
+  removeKeys(deletedItemKeys: string[]): (cursor?: string | number, ...cursors: (string | number)[]) => this {
     return this.cursorBuilder("batch", this.#value, (arr: any) => _removeKeys(arr, deletedItemKeys))
   }
 
-  replaceItem<T>(filter: (item: T, index: number) => boolean, newItem: T): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => this {
+  replaceItem<T>(filter: (item: T, index: number) => boolean, newItem: T): (cursor?: string | number, ...cursors: (string | number)[]) => this {
     return this.cursorBuilder("batch", this.#value, (arr: any) => _replaceItem(arr, filter, newItem))
   }
 
@@ -103,78 +71,45 @@ export default class m {
     return this.#value
   }
 
-  static push(array: any, value?: any, ...values: any[]): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => any {
+  static push(array: any, value?: any, ...values: any[]): (cursor?: string | number, ...cursors: (string | number)[]) => any {
     return cursorBuilder("push", array, value, ...values)
   }
-  static unshift(array: any, value?: any, ...values: any[]): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => any {
+  static unshift(array: any, value?: any, ...values: any[]): (cursor?: string | number, ...cursors: (string | number)[]) => any {
     return cursorBuilder("unshift", array, value, ...values)
   }
-  static removeKeys(arrayOrObj: any, deletedItemKeys: string[]): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => any {
+  static removeKeys(arrayOrObj: any, deletedItemKeys: string[]): (cursor?: string | number, ...cursors: (string | number)[]) => any {
     return cursorBuilder("batch", arrayOrObj, (arrOrObj: any) => _removeKeys(arrOrObj, deletedItemKeys))
   }
-  static replaceItem<T>(arrayOrObj: any, filter: (item: T, index: number) => boolean, newItem: T): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => any {
+  static replaceItem<T>(arrayOrObj: any, filter: (item: T, index: number) => boolean, newItem: T): (cursor?: string | number, ...cursors: (string | number)[]) => any {
     return cursorBuilder("batch", arrayOrObj, (arrOrObj: any) => _replaceItem(arrOrObj, filter, newItem))
   }
-  static splice(array: any, index: number, deleteCount: number, value?: any, ...values: any[]): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => any {
+  static splice(array: any, index: number, deleteCount: number, value?: any, ...values: any[]): (cursor?: string | number, ...cursors: (string | number)[]) => any {
     return cursorBuilder("splice", array, index, deleteCount, value, ...values)
   }
-  static unset(obj: any, index: number | string, ...indexs: (string | number | FilterByItem)[]): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => any {
+  static unset(obj: any, index: number | string, ...indexs: (string | number)[]): (cursor?: string | number, ...cursors: (string | number)[]) => any {
     return cursorBuilder("unset", obj, index, ...indexs)
   }
-  static set(obj: any, value: any): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => any {
+  static set(obj: any, value: any): (cursor?: string | number, ...cursors: (string | number)[]) => any {
     return cursorBuilder("set", obj, value)
   }
-  static update(obj: any, callback: (lastValue: any) => any): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => any {
+  static update(obj: any, callback: (lastValue: any) => any): (cursor?: string | number, ...cursors: (string | number)[]) => any {
     return cursorBuilder("batch", obj, callback)
   }
-  static assign(obj: any, obj1: any): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => any {
+  static assign(obj: any, obj1: any): (cursor?: string | number, ...cursors: (string | number)[]) => any {
     return cursorBuilder("assign", obj, deepCopy(obj1))
   }
 }
 
-
-
-function cursorBuilder(command: string, array: any, value: any, ...values: any[]): (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) => any {
-  return function (cursor?: string | number | FilterByItem, ...cursors: (string | number | FilterByItem)[]) {
-    let _value = array
-    let cCursor = [cursor, ...cursors]
-    let spec = {}
-    let hasError = false
-
-    cCursor = [cursor, ...cursors].map((item) => {
-      if (typeof item == 'string') {
-        _value = _value[item]
-        return item
-      } else if (typeof item == 'function') {
-        if (Array.isArray(_value)) {
-          const idx = _value.findIndex(item)
-          if (idx > -1) {
-            _value = _value[idx]
-            return idx
-          } else {
-            hasError = true
-            return undefined
-          }
-        } else {
-          hasError = true
-          return undefined
-          // new Error("LibObject : CursorByFilter must be executed at array")
-        }
-      }
-    });
-
-    let pathToUpdate = cCursor.filter(x => x != undefined).join('.')
+function cursorBuilder(command: string, array: any, value: any, ...values: any[]): (cursor?: string | number, ...cursors: (string | number)[]) => any {
+  return function (cursor?: string | number, ...cursors: (string | number)[]) {
+    let pathToUpdate = [cursor, ...cursors].filter(x => x != undefined).join('.')
     let allValues = [value, ...values].filter(x => x != undefined)
+    let spec = {}
     if (pathToUpdate != '')
       spec = { [pathToUpdate]: [command, ...allValues] }
     else
       spec = [command, ...allValues]
-
-    if (hasError) {
-      console.warn("LibObject: Please check your cursor!")
-      return array
-    } else
-      return update(array, spec)
+    return update(array, spec)
   }
 }
 
