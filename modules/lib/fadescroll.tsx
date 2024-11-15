62d951a1e19fc4c5ed8d94fc0b80cc432f03a835
kholil <kholil@fisip.net>
2024-11-15 10:43:32 +0700

    add lib/fadescroll

---
+++ modules/lib/fadescroll.tsx
@@ -0,0 +1,50 @@
+// withHooks
+
+import useSafeState from 'esoftplay/state';
+import React, { ReactElement } from 'react';
+import { ViewStyle } from 'react-native';
+import Animated, { Extrapolation, interpolate, runOnJS, SharedValue, useAnimatedReaction, useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
+
+
+export interface LibFadescrollArgs {
+
+}
+
+export interface LibFadescrollProps {
+  scrollController: SharedValue<number>,
+  style?: ViewStyle,
+  children?: ReactElement,
+  interpolateOpacity: number[],
+  interpolateScroll: number[],
+}
+export default function m(props: LibFadescrollProps): any {
+  let opacity = useSharedValue(0)
+  const [pointerEvents, setPointerEvents] = useSafeState<any>('none')
+  const { scrollController, interpolateScroll, interpolateOpacity } = props
+
+  // Animated style for opacity based on external scrollController value
+  const animatedStyle = useAnimatedStyle(() => {
+    opacity.value = interpolate(
+      scrollController.value,
+      interpolateScroll,
+      interpolateOpacity,
+      Extrapolation.CLAMP
+    );
+
+    return { opacity: opacity.value };
+  });
+
+  useAnimatedReaction(
+    () => opacity.value,
+    (currentValue) => {
+      runOnJS(setPointerEvents)(currentValue == 1 ? 'auto' : 'none');
+    },
+    []
+  );
+
+  return (
+    <Animated.View style={[props.style, animatedStyle]} pointerEvents={pointerEvents}>
+      {props.children}
+    </Animated.View>
+  );
+}
