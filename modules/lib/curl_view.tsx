790db963798ab688f5e5422debfbe940339e7a37
kholil <kholil@fisip.net>
2025-01-06 16:59:00 +0700

    add curl_view

---
+++ modules/lib/curl_view.tsx
@@ -0,0 +1,68 @@
+// withHooks
+
+import { ComponentMessage } from 'esoftplay/cache/component/message/import';
+import { LibCurl } from 'esoftplay/cache/lib/curl/import';
+import { LibLoading } from 'esoftplay/cache/lib/loading/import';
+import { LibUtils } from 'esoftplay/cache/lib/utils/import';
+import { UserData } from 'esoftplay/cache/user/data/import';
+import FastStorage from 'esoftplay/mmkv';
+import useSafeState from 'esoftplay/state';
+import React, { ReactElement, useEffect } from 'react';
+
+
+export interface LibCurl_viewArgs {
+
+}
+
+export interface LibCurl_viewProps {
+  url: string;
+  post?: any;
+  cache?: boolean;
+  isUserData?: boolean;
+  onSuccess?: (res: any, message: string) => ReactElement;
+  onError?: (err: any, retry: Function) => ReactElement;
+  onLoading?: ReactElement;
+}
+export default function m(props: LibCurl_viewProps): any {
+
+  let key = ""
+  let initialData: any = undefined
+  if (props.cache) {
+    
+    key = "curl-view" + LibUtils.shorten(props.url + JSON.stringify(props.post))
+    if (props.isUserData) {
+      UserData.register(key)
+    }
+    try {
+      initialData = JSON.parse(FastStorage.getItemSync(key))
+    } catch (error) { }
+  }
+
+  const [data, setData] = useSafeState(initialData)
+
+  function fetchData() {
+    new LibCurl(props.url, props.post, (result, message) => {
+      if (props.cache) {
+        FastStorage.setItem(key, JSON.stringify({ result, message }))
+      }
+      setData({ result, message })
+    }, (err) => {
+      setData(err)
+    })
+  }
+
+  function retry() {
+    setData(initialData)
+    fetchData()
+  }
+
+  useEffect(fetchData, []);
+
+  if (!data) {
+    return props.onLoading ? props.onLoading : <LibLoading />
+  }
+  if (data.ok == 0) {
+    return props.onError ? props.onError(data, retry) : <ComponentMessage message={data.message} />
+  }
+  return props.onSuccess ? props.onSuccess(data.result, data.message) : null
+}
--- modules/user/data.ts
+++ modules/user/data.ts
@@ -1,3 +1,4 @@
+//[moved] add new function multiregister
 // noPage
 // withObject
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -5,30 +6,51 @@ import { userDataReset } from 'esoftplay/global';
 import FastStorage from 'esoftplay/mmkv';
 import Storage from 'esoftplay/storage';
 
+(() => {
+  AsyncStorage.getItem("user_data_dependent").then((keys) => {
+    if (keys) {
+      FastStorage.setItem("user_data_dependent", keys)
+      AsyncStorage.removeItem("user_data_dependent")
+    }
+  })
+})()
+
 /** Klik [disini](https://github.com/dev-esoftplay/mobile-docs/blob/main/modules/user/data.md) untuk melihat dokumentasi*/
 export default {
-/** Klik [disini](https://github.com/dev-esoftplay/mobile-docs/blob/main/modules/user/data.md#register) untuk melihat dokumentasi*/
+  /** Klik [disini](https://github.com/dev-esoftplay/mobile-docs/blob/main/modules/user/data.md#register) untuk melihat dokumentasi*/
   register(name: string): void {
-    AsyncStorage.getItem("user_data_dependent").then((x) => {
-      let all: string[] = []
-      if (x) {
-        all = JSON.parse(x).filter((x) => x != name)
-      }
-      all.push(name)
-      AsyncStorage.setItem("user_data_dependent", JSON.stringify(all))
-    })
+    const x = FastStorage.getItemSync("user_data_dependent")
+    let all: string[] = []
+    if (x) {
+      all = JSON.parse(x).filter((x) => x != name)
+    }
+    all.push(name)
+    FastStorage.setItem("user_data_dependent", JSON.stringify(all))
+  },
+  async multiRegister(names: string[]): Promise<void> {
+    try {
+      const data = await FastStorage.getItem("user_data_dependent");
+      const all = data ? JSON.parse(data) : [];
+
+      const uniqueNames = names.filter((name) => !all.includes(name));
+      const updatedAll = [...all, ...uniqueNames];
+
+      await FastStorage.setItem("user_data_dependent", JSON.stringify(updatedAll));
+    } catch (error) {
+      console.error("Error registering multiple names:", error);
+    }
   },
   unregister(name: string): void {
-    AsyncStorage.getItem("user_data_dependent").then((x) => {
+    FastStorage.getItem("user_data_dependent").then((x) => {
       let all = []
       if (x) {
         all = JSON.parse(x).filter((x) => x != name)
       }
-      AsyncStorage.setItem("user_data_dependent", JSON.stringify(all))
+      FastStorage.setItem("user_data_dependent", JSON.stringify(all))
     })
   },
   deleteAll(): void {
-    AsyncStorage.getItem("user_data_dependent").then((x) => {
+    FastStorage.getItem("user_data_dependent").then((x) => {
       if (x) {
         userDataReset.forEach((f) => f())
         const arx = JSON.parse(x)
