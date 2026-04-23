b1a26725c44b2a0e76ed0e864ce9f5af9017fe30
kholil <kholil@fisip.net>
2026-04-23 16:28:42 +0700

    fix lib/focus

--- modules/lib/focus.tsx
+++ modules/lib/focus.tsx
@@ -2,7 +2,7 @@
 // noPage
 
 import { useIsFocused } from '@react-navigation/native';
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { InteractionManager, View, ViewStyle } from 'react-native';
 
 export interface LibFocusProps {
@@ -21,10 +21,10 @@ export default function m(props: LibFocusProps): any {
   const isFocused = useIsFocused()
 
   useEffect(() => {
-    if (isFocused) {
-      InteractionManager.runAfterInteractions(props?.onFocus)
-    } else {
-      InteractionManager.runAfterInteractions(props?.onBlur)
+    const callback = isFocused ? props.onFocus : props.onBlur
+    if (typeof callback === "function") {
+      const task = InteractionManager.runAfterInteractions(callback)
+      return () => task.cancel()
     }
   }, [isFocused]);
 
--- publisher_beta.js
+++ publisher_beta.js
@@ -5,7 +5,7 @@ const crypto = require('crypto')
 const version = packJson.version
 const baseVersion = version.split('-')[0]
 const randomHash = crypto.randomBytes(4).toString('hex').substring(0,7)
-const nextVersion = baseVersion + '-' + randomHash
+const nextVersion = baseVersion + '-beta' + randomHash
 const newPackJson = { ...packJson, version: nextVersion }
 fs.writeFileSync("./package.json", JSON.stringify(newPackJson, undefined, 2))
 shell("npm publish", { stdio: ['inherit', 'inherit', 'inherit'] })
